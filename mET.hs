ac405ad6cf167978c795147c61df2209bb8f7d10
Luze26 <yann.uzel@gmail.com>
2013-01-07 11:11:39 +0100

    Refactoring

--- mET.hs
+++ mET.hs
@@ -5,43 +5,51 @@ import System.IO
 import System.Environment( getArgs )
 
 
+-- main ////////////////////////////////////////////////////////////////
 main = do
 	args <- getArgs
-	handle <- openFile (args !! 0) ReadMode  
-	contents <- hGetContents handle  
+	contents <- readFile (args !! 0)
 	play 0 $ createList contents
-	hClose handle
 
 
-
+-- play ////////////////////////////////////////////////////////////////
 play :: Int -> [[String]] -> IO ()
 play  i [] = do putStrLn $ "Done in " ++ ((intToDigit i) : " shots !!")
 play i list = do
 		rand <- randomRIO (0 :: Int, length list - 1)
-		putStrLn $ drawWord rand list
-		l <- getLine
-		let solutions = tail $ list !! rand in do
-		putStrLn $ foldl (\deck x -> "\n+" ++ x ++ deck) "" $ solutions
-		if correct l $ solutions
-			then do
-				putStrLn ":)" 
-				play (i+1) $ let (ys,zs) = splitAt rand list in ys ++ (tail zs)
-			else do
-				putStrLn ":(" 
-				play (i+1) list
-		return ()
+		let 	tuple = list !! rand 
+			solutions = tail tuple
+			in do
+				putStrLn $ head tuple
+				answer <- getLine
+				if correct answer tuple
+				then do
+					putStrLn $ response True solutions
+					play (i+1) $ let (ys,zs) = splitAt rand list in ys ++ (tail zs)
+				else do
+					putStrLn $ response False solutions
+					play (i+1) list
+				return ()
 
+
+-- createList ////////////////////////////////////////////////////////////////
 createList :: String -> [[String]]
 createList input = [ map T.unpack $ head x : (T.splitOn slash $ last x) | x <- map (T.splitOn tab) $ map T.pack $ lines input]
 	where tab = T.pack "\t"
 	      slash = T.pack "/"
 
-
-drawWord :: Int -> [[String]] -> String
-drawWord r list = list !! r !! 0
-
+	      
+-- correct ////////////////////////////////////////////////////////////////
 correct :: String -> [String] -> Bool
 correct _ [] = False
 correct rep (x:xs)
 	| rep==x = True
 	| otherwise = correct rep xs
+
+
+-- response ////////////////////////////////////////////////////////////////
+response :: Bool -> [String] -> String
+response b solutions
+	| b == True = "=)" ++ solutionsString
+	| otherwise = "=\\" ++ solutionsString
+	where solutionsString = foldl (\deck sol -> "\n\t-" ++ sol ++ deck) "" solutions
